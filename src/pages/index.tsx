75b9b7d75cfe01248237dfdbe575d7d7359d8c59
ArekRGW <arkadiusz.pawlak.web@gmail.com>
2021-11-19 09:44:14 +0100

    page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
+import PageLayout from "@components/PageLayout";
 import { Typography } from "@mui/material";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <Typography variant="h1">Personager</Typography>;
+  return (
+    <PageLayout>
+      <Typography variant="h1">Personager</Typography>
+    </PageLayout>
+  );
 };
 
 export default Home;
